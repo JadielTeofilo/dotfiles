1d795c6bc5404fb2d3be2cb3427c950d91388560
jadiel <teofilojadiel@gmail.com>
2021-07-17 11:25:12 -0300

    update stuff

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,7 +1,10 @@
-xrandr -s 1920x1080
-i3-msg 'workspace 1:Web; exec /usr/bin/firefox'
-sleep 5
-i3-msg 'workspace 2:Code; exec /usr/bin/pycharm'
-sleep 34
-i3-msg 'workspace 3:Term; exec /usr/bin/terminator'
-clipster -d
+xrandr --output HDMI-1 --brightness 0.6
+i3-msg 'workspace 1: Media ; exec /usr/bin/firefox'
+sleep 1
+i3-msg 'workspace 2: Code ; exec code'
+sleep 1
+i3-msg 'workspace 3: Term ; exec /usr/bin/terminator'
+nohup clipster -d &
+nohup guake &
+sleep 1
+i3-msg 'workspace 2: Code '
