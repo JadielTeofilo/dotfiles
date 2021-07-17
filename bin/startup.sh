fa4dc838e9196e678c9e9a25db54ace95354aaef
jadiel <teofilojadiel@gmail.com>
2021-07-17 12:06:52 -0300

    add configs

--- bin/startup.sh
+++ bin/startup.sh
@@ -6,5 +6,7 @@ sleep 1
 i3-msg 'workspace 3: Term ; exec /usr/bin/terminator'
 nohup clipster -d &
 nohup guake &
+setxkbmap -layout us,br
+setxkbmap -option 'grp:alt_shift_toggle'
 sleep 1
 i3-msg 'workspace 2: Code '
---
+++ update_configs.sh
@@ -0,0 +1,2 @@
+cp -R ~/.config/i3 .config/
+sudo cp /bin/startup.sh bin/startup.sh
