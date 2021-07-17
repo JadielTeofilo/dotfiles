cecd4c18797399061a40eeee204adfee74a3be11
jadiel <teofilojadiel@gmail.com>
2021-07-17 16:33:49 -0300

    updating conf sáb 17 jul 2021 16:33:49 -03

--- update_configs.sh
+++ update_configs.sh
@@ -1,7 +1,6 @@
 cp -R ~/.config/i3 .config/
 cp ~/.config/i3blocks.conf .config/i3blocks.conf
-sudo cp /usr/share/i3blocks/volume usr/share/i3blocks
-sudo cp /bin/startup.sh bin/startup.sh
+sudo cp /usr/share/i3blocks/mic usr/share/i3blocks
 sudo cp /bin/startup.sh bin/startup.sh
 git add -A
 git commit -m "updating conf $(date)"
